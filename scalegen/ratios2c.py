73b57ee0870fb711072ce9e14127948f3cbc01e7
Will <liquidcitymotors@gmail.com>
2018-03-19 03:30:08 -0500

    feeds new pll firmware with 5limit just chromatic scale

--- scalegen/ratios2c.py
+++ scalegen/ratios2c.py
@@ -7,7 +7,7 @@ from math import gcd
 text_file = open("chromatic5prime.txt", "a")
 
 unison5prime = np.array([1, 1])
-min_second5prime = np.array([15, 16])
+min_second5prime = np.array([16, 15])
 maj_second5prime = np.array([9, 8])
 min_third5prime = np.array([6, 5])
 maj_third5prime = np.array([5, 4])
@@ -32,35 +32,40 @@ chromatic5prime_tags = ("unison5prime", "min_second5prime", "maj_second5prime",
 
 for i in range(0, 128):
 
-    octave = int((i - 64)/12)
+    octave = int((i - 64)//12)
 
-    numerator_int = int(chromatic5prime_ratios[(i - 64) % 12][0])
-    denominator_int = int(chromatic5prime_ratios[(i - 64) % 12][1])
+    numerator_int = int(chromatic5prime_ratios[(i + 8) % 12][0])
+    denominator_int = int(chromatic5prime_ratios[(i + 8) % 12][1])
 
-    if octave > 0:
+    if octave >= 0:
         temp_numerator = numerator_int * 2**octave
         divisor = gcd(int(temp_numerator), int(denominator_int))
         fundamental_divisor = int(denominator_int/divisor)
+        ratio_tag = chromatic5prime_tags[(i - 64) % 12] + str(octave)
+
     else:
         temp_denominator = denominator_int * 2**(-octave)
         divisor = gcd(int(numerator_int), int(temp_denominator))
         fundamental_divisor = int(temp_denominator/divisor)
+        ratio_tag = chromatic5prime_tags[(i - 64) % 12] + "_" + str(abs(octave))
 
-    fix32_calculation = int((numerator_int * 2**(32 + octave))/denominator_int)
+    fix32_calculation = int((numerator_int * 2**(48 + octave))/denominator_int)
 
     integer_part = fix32_calculation >> 32
 
-    fractional_part = fix32_calculation - integer_part
-
-    ratio_tag = chromatic5prime_tags[(i - 64) % 12] + str(octave)
+    fractional_part = fix32_calculation - (integer_part << 32)
 
     text_file.write("#define " + ratio_tag + " {" + str(integer_part) + ", " + str(fractional_part) + ", " + str(fundamental_divisor) + "}\n")
 
 for i in range(0, 128):
 
-    octave = int((i - 64) / 12)
+    octave = int((i - 64) // 12)
 
-    ratio_tag = chromatic5prime_tags[(i - 64) % 12] + str(octave)
+    if octave >= 0:
+        ratio_tag = chromatic5prime_tags[(i - 64) % 12] + str(octave)
+
+    else:
+        ratio_tag = chromatic5prime_tags[(i - 64) % 12] + "_" + str(abs(octave))
 
     if i == 0:
         text_file.write("const ScaleNote chromatic5prime[128] = {" + ratio_tag + ", ")
