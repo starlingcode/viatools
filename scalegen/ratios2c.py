4e89605bebd6135b38f3ed77eeffc96b5feaa4cd
Will <liquidcitymotors@gmail.com>
2018-03-21 01:17:49 -0500

    halfway through scale parser, needs fuction to fill scale with tiles

--- scalegen/ratios2c.py
+++ scalegen/ratios2c.py
@@ -2,19 +2,21 @@
 
 import numpy as np
 
-from math import gcd
+import math
 
 import csv
 
+#initialize lists to parse the csv
 ratio_subset = []
 ratio_table = []
+interval_subset = []
+interval_table = []
 scale_tags = []
 
 
-scale_name = "modal5prime"
+scale_name = "harmSubharm"
 
-
-
+#parse the csv for
 with open(scale_name + ".csv", newline="\n") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
@@ -23,20 +25,96 @@ with open(scale_name + ".csv", newline="\n") as csvfile:
                 scale_tags.append(row[0])
                 print(row[0])
             else:
-                ratio = (int(row[0]), int(row[1]))
+                ratio = [int(row[0]), int(row[1])]
                 ratio_subset.append(ratio)
+                interval_subset.append(math.log(float(ratio[0]/ratio[1]), 2))
 
         elif ratio_subset != []:
             ratio_table.append(ratio_subset)
             ratio_subset = []
+            interval_table.append(interval_subset)
+            interval_subset = []
+
+#Calculate octave bin for each interval in each scale and append to the older
+
+row_pointer = 0
+octave_spans = []
+
+for i in interval_table:
+
+    octave_checker = 0
+
+    while (max(interval_table[row_pointer]) - min(interval_table[row_pointer])) > octave_checker:
+
+        octave_checker = octave_checker + 1
+
+    interval_pointer = 0
+
+    for j in interval_table[row_pointer]:
+
+        ratio_table[row_pointer][interval_pointer].append(int(12*interval_table[row_pointer][interval_pointer]))
+        # tuple(ratio_table[row_pointer][interval_pointer])
+        interval_pointer = interval_pointer + 1
+
+    ratio_table[row_pointer].sort(key=lambda x: int(x[2]))
+
+    octave_spans.append(octave_checker)
+    row_pointer = row_pointer + 1
 
 
+print(interval_table)
 print(ratio_table)
 print(scale_tags)
+print(octave_spans)
+
+start_end_table = []
+start_end_subset = []
+
+for i in ratio_table:
+
+    row_pointer = ratio_table.index(i)
+
+    lower_bound = 8
+
+    while (min(interval_table[row_pointer])) < lower_bound:
+
+        lower_bound = lower_bound - 1
+
+    pad = -lower_bound*12
+
+    for j in ratio_table[row_pointer]:
+
+            interval_pointer = ratio_table[row_pointer].index(j)
+
+            if interval_pointer == 0:
+
+                start = 0;
+                end = pad + int((ratio_table[row_pointer][interval_pointer][2] + ratio_table[row_pointer][interval_pointer + 1][2])/2)
+                start_end_subset.append([start, end])
+
+            elif interval_pointer != (len(ratio_table[row_pointer]) - 1):
+
+                start = start_end_subset[interval_pointer - 1][1] + 1;
+                end = pad + int((ratio_table[row_pointer][interval_pointer][2] + ratio_table[row_pointer][interval_pointer + 1][2])/2)
+                start_end_subset.append([start, end])
+
+            else:
+
+                start = start_end_subset[interval_pointer - 1][1] + 1;
+                end = octave_spans[row_pointer]*12 - 1
+                start_end_subset.append([start, end])
+
+    start_end_table.append(start_end_subset)
+    start_end_subset = []
 
 
-text_file = open("modal5prime.txt", "a")
+print(start_end_table)
+
+
+text_file = open(scale_name + ".txt", "a")
 text_file.truncate()
+
+#####################
 
 calculated_table = []
 pitch_class_set = set([])
@@ -54,13 +132,13 @@ for i in range(0, 8):
     
         if octave >= 0:
             temp_numerator = numerator_int * 2**octave
-            divisor = gcd(int(temp_numerator), int(denominator_int))
+            divisor = math.gcd(int(temp_numerator), int(denominator_int))
             fundamental_divisor = int(denominator_int/divisor)
             ratio_tag = "ratio" + str(temp_numerator) + "_" + str(denominator_int)
     
         else:
             temp_denominator = denominator_int * 2**(-octave)
-            divisor = gcd(int(numerator_int), int(temp_denominator))
+            divisor = math.gcd(int(numerator_int), int(temp_denominator))
             fundamental_divisor = int(temp_denominator/divisor)
             ratio_tag = "ratio" + str(numerator_int) + "_" + str(temp_denominator)
     
@@ -81,9 +159,6 @@ for i in range(0, 8):
     calculated_table.append(calculated_row)
 
 print(len(calculated_table))
-
-
-
 
 for i in pitch_class_set:
     ratio_tag = i[0]
