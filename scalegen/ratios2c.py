9b9ba51a949964056e0a004489a4ba40642c00f2
Will <liquidcitymotors@gmail.com>
2018-03-22 01:55:23 -0500

    rough working scale generator

--- scalegen/ratios2c.py
+++ scalegen/ratios2c.py
@@ -16,7 +16,10 @@ scale_tags = []
 
 scale_name = "harmSubharm"
 
-#parse the csv for
+is_1voct = 0;
+
+
+#parse the csv for integer ratios and calculate an interval in terms of octaves
 with open(scale_name + ".csv", newline="\n") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
@@ -35,25 +38,35 @@ with open(scale_name + ".csv", newline="\n") as csvfile:
             interval_table.append(interval_subset)
             interval_subset = []
 
-#Calculate octave bin for each interval in each scale and append to the older
+#Calculate octave bin for each interval in each scale and append to the first table
+
+print(len(ratio_table))
+print(len(interval_subset))
 
 row_pointer = 0
 octave_spans = []
 
 for i in interval_table:
 
-    octave_checker = 0
+    octave_checker_positive = -8
 
-    while (max(interval_table[row_pointer]) - min(interval_table[row_pointer])) > octave_checker:
+    while max(interval_table[row_pointer]) > octave_checker_positive:
 
-        octave_checker = octave_checker + 1
+        octave_checker_positive = octave_checker_positive + 1
+
+    octave_checker_negative = 8
+
+    while min(interval_table[row_pointer]) < octave_checker_negative:
+
+        octave_checker_negative = octave_checker_negative - 1
+
+    octave_checker = octave_checker_positive - octave_checker_negative
 
     interval_pointer = 0
 
     for j in interval_table[row_pointer]:
 
         ratio_table[row_pointer][interval_pointer].append(int(12*interval_table[row_pointer][interval_pointer]))
-        # tuple(ratio_table[row_pointer][interval_pointer])
         interval_pointer = interval_pointer + 1
 
     ratio_table[row_pointer].sort(key=lambda x: int(x[2]))
@@ -66,6 +79,10 @@ print(interval_table)
 print(ratio_table)
 print(scale_tags)
 print(octave_spans)
+
+num_scales = len(scale_tags)
+
+#get start and end indices for each scale in the n octave tile of best fit
 
 start_end_table = []
 start_end_subset = []
@@ -110,74 +127,139 @@ for i in ratio_table:
 
 print(start_end_table)
 
+# generate the n octave sized tile for each row using the indices from above
 
-text_file = open(scale_name + ".txt", "a")
-text_file.truncate()
+subtile = []
+tiles = []
 
-#####################
+for i in ratio_table:
 
-calculated_table = []
+    row_pointer = ratio_table.index(i)
+
+    for j in ratio_table[row_pointer]:
+
+        interval_pointer = ratio_table[row_pointer].index(j)
+
+        starting_index = start_end_table[row_pointer][interval_pointer][0]
+
+        last_index = start_end_table[row_pointer][interval_pointer][1]
+
+        idx = starting_index
+
+        while idx <= last_index:
+
+            subtile.append(tuple(ratio_table[row_pointer][interval_pointer][0:2]))
+            idx += 1;
+
+
+    tiles.append(subtile)
+
+    subtile = []
+
+print(tiles)
+
+full_scale = []
+full_row = []
 pitch_class_set = set([])
 
-for i in range(0, 8):
+for i in tiles:
 
-    calculated_row = []
+    row_pointer = tiles.index(i)
 
-    for j in range(0, 128):
+    j = 0
     
-        octave = int((j - 64)//12)
-    
-        numerator_int = int(ratio_table[i][(j + 8) % 12][0])
-        denominator_int = int(ratio_table[i][(j + 8) % 12][1])
-    
-        if octave >= 0:
-            temp_numerator = numerator_int * 2**octave
-            divisor = math.gcd(int(temp_numerator), int(denominator_int))
-            fundamental_divisor = int(denominator_int/divisor)
-            ratio_tag = "ratio" + str(temp_numerator) + "_" + str(denominator_int)
-    
-        else:
-            temp_denominator = denominator_int * 2**(-octave)
-            divisor = math.gcd(int(numerator_int), int(temp_denominator))
-            fundamental_divisor = int(temp_denominator/divisor)
-            ratio_tag = "ratio" + str(numerator_int) + "_" + str(temp_denominator)
-    
-        fix32_calculation = int((numerator_int * 2**(48 + octave))/denominator_int)
+    while j < 64:
+        
+        octave = int(j // len(i)) * octave_spans[row_pointer]
+
+        numerator_int = int(tiles[row_pointer][j % len(i)][0])
+        
+        denominator_int = int(tiles[row_pointer][j % len(i)][1])
+        
+        temp_numerator = numerator_int * 2 ** octave
+        
+        divisor = math.gcd(int(temp_numerator), int(denominator_int))
+        
+        fundamental_divisor = int(denominator_int / divisor)
+        
+        ratio_tag = "ratio" + str(temp_numerator) + "_" + str(denominator_int)
+            
+        fix32_calculation = int(temp_numerator * 2**48/denominator_int)
     
         integer_part = fix32_calculation >> 32
     
         fractional_part = fix32_calculation - (integer_part << 32)
 
         ratio_holder = (ratio_tag, integer_part, fractional_part, fundamental_divisor)
-
-        calculated_row.append(ratio_holder)
-
+        
         if ratio_holder not in pitch_class_set:
             pitch_class_set.add(ratio_holder)
 
-    print(len(calculated_row))
-    calculated_table.append(calculated_row)
+        full_row.append(ratio_holder)
 
-print(len(calculated_table))
+        j += 1
+    
+    j = 63
+
+    while j >= 0:
+        octave = abs((j-64) // len(i)) * octave_spans[row_pointer]
+
+        numerator_int = int(tiles[row_pointer][(len(i) - (64 - j)) % len(i)][0])
+
+        denominator_int = int(tiles[row_pointer][(len(i) - (64 - j)) % len(i)][1])
+
+        temp_denominator = denominator_int * 2 ** octave
+
+        divisor = math.gcd(int(numerator_int), int(temp_denominator))
+
+        fundamental_divisor = int(temp_denominator / divisor)
+
+        ratio_tag = "ratio" + str(numerator_int) + "_" + str(temp_denominator)
+
+        fix32_calculation = int(numerator_int*2**48 / temp_denominator)
+
+        integer_part = fix32_calculation >> 32
+
+        fractional_part = fix32_calculation - (integer_part << 32)
+
+        ratio_holder = (ratio_tag, integer_part, fractional_part, fundamental_divisor)
+
+        full_row.insert(0, ratio_holder)
+
+        j -= 1
+        
+        if ratio_holder not in pitch_class_set:
+            pitch_class_set.add(ratio_holder)
+
+    full_scale.append(full_row)
+    full_row = []
+
+
+text_file = open(scale_name + ".txt", "a")
+text_file.truncate()
+
+#####################
+
+text_file.write("const ScaleNote *" + scale_name + "[8];\n\n")
 
 for i in pitch_class_set:
     ratio_tag = i[0]
     integer_part = i[1]
     fractional_part = i[2]
     fundamental_divisor = i[3]
-    text_file.write("const ScaleNote " + ratio_tag + " =  {" + str(integer_part) + ", " + str(fractional_part) + ", " + str(fundamental_divisor) + "}\n")
+    text_file.write("#define " + ratio_tag + " {" + str(integer_part) + ", " + str(fractional_part) + ", " + str(fundamental_divisor) + "}\n")
 
 print(len(pitch_class_set))
 
+print(len(full_scale[0]))
+
 text_file.write("\n\n\n")
 
-
-
-for i in range(0,8):
+for i in range(0,num_scales):
 
     for j in range(0, 128):
 
-        ratio_tag = str(calculated_table[i][j][0])
+        ratio_tag = str(full_scale[i][j][0])
 
         if j == 0:
             text_file.write("const ScaleNote " + scale_tags[i] + "[128] = {" + ratio_tag + ", ")
@@ -194,8 +276,8 @@ text_file.write("\n\n\n")
 for i in range(len(scale_tags)):
 
     if i == 0:
-        text_file.write("const ScaleNote " + scale_name + "[128] = {" + scale_tags[i] + ", ")
-    elif i != 7:
+        text_file.write("const ScaleNote *" + scale_name + "[8] = {" + scale_tags[i] + ", ")
+    elif i != (len(scale_tags) - 1):
         text_file.write(scale_tags[i] + ", ")
     else:
         text_file.write(scale_tags[i] + "}; \n\n")
