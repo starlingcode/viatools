933b67214b452ed2ed374ec0eff8d585db974ed5
Will <liquidcitymotors@gmail.com>
2018-05-01 01:46:45 -0500

    updated trigger seq gen

--- euclidean_generator/patterns.h
+++ euclidean_generator/patterns.h
@@ -5,43 +5,62 @@
 
 
 typedef struct {
-	const uint32_t *aPattern;
-	const uint32_t *bPattern;
-	const uint32_t aLength;
-	const uint32_t bLength;
+	const uint32_t *data;
+	const uint32_t length;
 } pattern;
 
 
+typedef struct {
+	const pattern *aPatternBank;
+	const pattern *bPatternBank;
+	const uint32_t aLength;
+	const uint32_t bLength;
+} pattern_bank;
 
-static const uint32_t eucludean5_16[16] = {1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0}; 
 
-static const uint32_t eucludean2_16[16] = {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}; 
 
-static const uint32_t eucludean2_8[8] = {1, 0, 0, 0, 1, 0, 0, 0}; 
+static const pattern euclidean_simple_3_8[8] = {{1, 0, 0, 1, 0, 0, 1, 0}, 8}; 
 
-static const uint32_t eucludean3_16[16] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}; 
+static const pattern euclidean_simple_8_16[16] = {{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}, 16}; 
 
-static const uint32_t eucludean5_8[8] = {1, 0, 1, 1, 0, 1, 1, 0}; 
+static const pattern euclidean_simple_8_8[8] = {{1, 1, 1, 1, 1, 1, 1, 1}, 8}; 
 
-static const uint32_t eucludean8_16[16] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}; 
+static const pattern euclidean_simple_6_8[8] = {{1, 1, 1, 0, 1, 1, 1, 0}, 8}; 
 
-static const uint32_t eucludean7_8[8] = {1, 1, 1, 1, 1, 1, 1, 0}; 
+static const pattern euclidean_simple_4_16[16] = {{1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0}, 16}; 
 
-static const uint32_t eucludean7_16[16] = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0}; 
+static const pattern euclidean_simple_4_8[8] = {{1, 0, 1, 0, 1, 0, 1, 0}, 8}; 
 
-static const uint32_t eucludean4_8[8] = {1, 0, 1, 0, 1, 0, 1, 0}; 
+static const pattern euclidean_simple_5_8[8] = {{1, 0, 1, 1, 0, 1, 1, 0}, 8}; 
 
-static const uint32_t eucludean4_16[16] = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0}; 
+static const pattern euclidean_simple_6_16[16] = {{1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0}, 16}; 
 
-static const uint32_t eucludean8_8[8] = {1, 1, 1, 1, 1, 1, 1, 1}; 
+static const pattern euclidean_simple_7_16[16] = {{1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0}, 16}; 
 
-static const uint32_t eucludean3_8[8] = {1, 0, 0, 1, 0, 0, 1, 0}; 
+static const pattern euclidean_simple_2_16[16] = {{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 16}; 
 
-static const uint32_t eucludean6_16[16] = {1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0}; 
+static const pattern euclidean_simple_5_16[16] = {{1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0}, 16}; 
 
-static const uint32_t eucludean6_8[8] = {1, 1, 1, 0, 1, 1, 1, 0}; 
+static const pattern euclidean_simple_3_16[16] = {{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, 16}; 
 
-static const uint32_t eucludean1_8[8] = {1, 0, 0, 0, 0, 0, 0, 0}; 
+static const pattern euclidean_simple_1_16[16] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 16}; 
 
-static const uint32_t eucludean1_16[16] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+static const pattern euclidean_simple_7_8[8] = {{1, 1, 1, 1, 1, 1, 1, 0}, 8}; 
+
+static const pattern euclidean_simple_1_8[8] = {{1, 0, 0, 0, 0, 0, 0, 0}, 8}; 
+
+static const pattern euclidean_simple_2_8[8] = {{1, 0, 0, 0, 1, 0, 0, 0}, 8}; 
+
+
+
+
+static const uint32_t *euclidean_simple_a[8] = {euclidean_simple_1_16, euclidean_simple_2_16, euclidean_simple_3_16, euclidean_simple_4_16, euclidean_simple_5_16, euclidean_simple_6_16, euclidean_simple_7_16, euclidean_simple_8_16}; 
+
+static const uint32_t *euclidean_simple_b[8] = {euclidean_simple_1_8, euclidean_simple_2_8, euclidean_simple_3_8, euclidean_simple_4_8, euclidean_simple_5_8, euclidean_simple_6_8, euclidean_simple_7_8, euclidean_simple_8_8}; 
+
+static const pattern_bank euclidean_simple = {
+   .aPatternBank = euclidean_simple_a,
+   .bPatternBank = euclidean_simple_b,
+   .aLength = 8,
+   .bLength = 8};
 
--- euclidean_generator/trigseqgen.py
+++ euclidean_generator/trigseqgen.py
@@ -40,29 +40,41 @@ def bjorklund(steps, pulses):
     pattern = pattern[i:] + pattern[0:i]
     return pattern
 
+def parse_csv_euclidean(bank_name, pattern_set):
 
-a_patterns = []
-b_patterns = []
-pattern_set = set([])
+    a_patterns = []
+    b_patterns = []
 
-with open("euclidean.csv", newline="\n") as csvfile:
-    euclidreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in euclidreader:
+    with open(bank_name + ".csv", newline="\n") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            if row[0] != "":
+                a_pattern = [bank_name + "_" + str(row[0]) + "_" + str(row[1]),
+                             tuple(bjorklund(row[1], row[0]))]
+                a_pattern = tuple(a_pattern)
+                a_patterns.append(a_pattern)
+                pattern_set.add(a_pattern)
+                b_pattern = [bank_name + "_" + str(row[2]) + "_" + str(row[3]),
+                             tuple(bjorklund(row[3], row[2]))]
+                b_pattern = tuple(b_pattern)
+                b_patterns.append(b_pattern)
+                pattern_set.add(b_pattern)
+    return [a_patterns, b_patterns, pattern_set]
+
+
+current_pattern_set = set([])
+banks = []
+
+with open("euclidean_banks.csv", newline="\n") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in reader:
         if row[0] != "":
-            a_pattern = ["eucludean" + str(row[0]) + "_" + str(row[1]),
-                              tuple(bjorklund(row[1], row[0]))]
-            a_pattern = tuple(a_pattern)
-            a_patterns.append(a_pattern)
-            pattern_set.add(a_pattern)
-            b_pattern = ["eucludean" + str(row[2]) + "_" + str(row[3]),
-                              tuple(bjorklund(row[3], row[2]))]
-            b_pattern = tuple(b_pattern)
-            b_patterns.append(b_pattern)
-            pattern_set.add(b_pattern)
+            results = parse_csv_euclidean(row[0], current_pattern_set)
+            current_pattern_set = results[2]
+            banks.append([row[0], results[0], results[1]])
 
-print(a_patterns)
-print(b_patterns)
-print(pattern_set)
+
+
 
 text_file = open("patterns.h", "w")
 text_file.truncate()
@@ -72,29 +84,66 @@ text_file.write('#include "stm32f3xx.h"\n')
 text_file.write('#include "stm32f3xx_it.h"\n')
 text_file.write("\n\n\n")
 text_file.write("typedef struct {\n")
-text_file.write("	const uint32_t *aPattern;\n")
-text_file.write("	const uint32_t *bPattern;\n")
+text_file.write("	const uint32_t *data;\n")
+text_file.write("	const uint32_t length;\n")
+text_file.write("} pattern;\n")
+text_file.write("\n\n")
+text_file.write("typedef struct {\n")
+text_file.write("	const pattern *aPatternBank;\n")
+text_file.write("	const pattern *bPatternBank;\n")
 text_file.write("	const uint32_t aLength;\n")
 text_file.write("	const uint32_t bLength;\n")
-text_file.write("} pattern;\n")
+text_file.write("} pattern_bank;\n")
 text_file.write("\n\n\n")
 
-for i in pattern_set:
+for i in current_pattern_set:
     tag = str(i[0])
     pattern = list(i[1])
     length = len(pattern)
-    print(pattern)
-    print(tag)
-    print(length)
     for index in range(0, len(pattern)):
         print(index)
         if index == 0:
             text_file.write(
-                "static const uint32_t " + tag + "[" + str(length) + "] = {" + str(pattern[0]) + ", ")
+                "static const pattern " + tag + "[" + str(length) + "] = {{" + str(pattern[0]) + ", ")
         elif index != (length - 1):
             text_file.write(str(pattern[index]) + ", ")
         else:
-            text_file.write(str(pattern[index]) + "}; \n\n")
+            text_file.write(str(pattern[index]) + "}, " + str(length) + "}; \n\n")
+text_file.write("\n\n\n")
+
+for i in banks:
+    a_length = len(i[1])
+    b_length = len(i[2])
+    for index in range(0, a_length):
+        print(index)
+        if index == 0:
+            text_file.write(
+                "static const uint32_t *" + i[0] + "_a[" + str(length) + "] = {" + str(i[1][0][0]) + ", ")
+        elif index != (a_length - 1):
+            text_file.write(str(i[1][index][0]) + ", ")
+        else:
+            text_file.write(str(i[1][index][0]) + "}; \n\n")
+
+    for index in range(0, b_length):
+        print(index)
+        if index == 0:
+            text_file.write(
+                "static const uint32_t *" + i[0] + "_b[" + str(length) + "] = {" + str(i[2][0][0]) + ", ")
+        elif index != (b_length - 1):
+            text_file.write(str(i[2][index][0]) + ", ")
+        else:
+            text_file.write(str(i[2][index][0]) + "}; \n\n")
+
+    text_file.write("static const pattern_bank " + i[0] + " = {\n")
+    text_file.write("   .aPatternBank = " + i[0] + "_a,\n")
+    text_file.write("   .bPatternBank = " + i[0] + "_b,\n")
+    text_file.write("   .aLength = " + str(a_length) + ",\n")
+    text_file.write("   .bLength = " + str(b_length) + "};\n\n")
+
+
+
+
+
 
 
 
