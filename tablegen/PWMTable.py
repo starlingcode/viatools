4212acb40b01460617a98bd4c14a920b4ff4056e
Will <liquidcitymotors@gmail.com>
2018-06-01 16:07:31 -0500

    update PWM gen script

--- tablegen/PWMTable.py
+++ tablegen/PWMTable.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-tableLength = 512 * (2**16)
+tableLength = (512 * (2**16)) - 1
 
-storeLookup = np.zeros(64)
+storeLookup = np.zeros(65)
 lookupTable = []
 
 # iterate through 64 duty cycle values
@@ -12,20 +12,57 @@ lookupTable = []
 # if phase < tableLength*PWM, phase = phase * (1/(2*PWM))
 # else, phase = phase * (1/(2*(1 -PWM)))
 
-PWMValues = np.linspace(0, 1, 64)
-phaseValues = np.linspace(0, tableLength, 64)
+PWMValues = np.linspace(0.2, .8, 33)
+phaseValues = np.linspace(0, tableLength, 65)
 
 for PWM in PWMValues:
     for phaseIndex, phase in np.ndenumerate(phaseValues):
         if phase < tableLength * PWM:
             coefficient = (.5/PWM)
-            storeLookup[phaseIndex] = phase * coefficient
+            storeLookup[phaseIndex] = int(phase * coefficient)
         else:
             coefficient = (.5/(1-PWM))
-            storeLookup[phaseIndex] = phase * coefficient + tableLength * (1 - coefficient)
+            storeLookup[phaseIndex] = int(phase * coefficient + tableLength * (1 - coefficient))
     lookupTable.append(storeLookup)
-    plt.plot(PWMValues, storeLookup)
-    storeLookup = np.zeros(64)
+    plt.plot(phaseValues, storeLookup)
+    storeLookup = np.zeros(65)
+
+############
+
+text_file = open("pwm_tables.h", "w")
+text_file.truncate()
+
+text_file.write('#ifndef PWM_TABLES"\n')
+text_file.write('#define PWM_TABLES"\n')
+
+text_file.write('#include "stm33f3xx_hal.h"\n')
+text_file.write('#include "stm33f3xx.h"\n')
+text_file.write('#include "stm33f3xx_it.h"\n')
+text_file.write('#include "dsp.h"\n')
+text_file.write("\n\n\n")
+
+tableIndex = 0
+while tableIndex < 33:
+    text_file.write("#define phaseModPWM_" + str(tableIndex) + " {" + str(int(lookupTable[tableIndex][0])))
+    sampleIndex = 1
+    while sampleIndex < 65:
+        text_file.write(", " + str(int(lookupTable[tableIndex][sampleIndex])))
+        sampleIndex += 1
+    text_file.write("}\n\n")
+    tableIndex += 1
+
+text_file.write("q31_t phaseModPWMTables[64][64] = {phaseModPWM_0")
+tableIndex = 1
+while tableIndex < 33:
+    text_file.write(", phaseModPWM_" + str(tableIndex))
+    tableIndex += 1;
+text_file.write("};\n\n")
+
+text_file.write('#endif\n')
+
+
+
+text_file.close()
 
 print(lookupTable)
 plt.show()
